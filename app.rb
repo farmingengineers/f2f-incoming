51a0d3679a89148508ce72421d4830454b740956
Matt Burke <spraints@gmail.com>
2015-05-09 17:23:10 -0400

    Set up the incoming hook path

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require "sinatra/base"
 
 class F2fIncomingApp < Sinatra::Base
+  hook_path = ENV["WEBHOOK_SECRET_PATH"] || "incoming"
+
+  get "/#{hook_path}" do
+    ":metal:\n"
+  end
+
   get "/*" do
     redirect "http://www.farmtoforkmarket.org/", 301
   end
